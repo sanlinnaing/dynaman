b508723845239d5f0e715943086cbfd2ee1a4856
San Lin Naing <sanlinnaing2008@gmail.com>
2026-01-03 23:28:49 +0900

    change depecrated utcnow() to datetime.now()

--- auth-service/domain/services/security_service.py
+++ auth-service/domain/services/security_service.py
@@ -1,5 +1,5 @@
 import bcrypt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from jose import jwt
 from config import settings
 
@@ -25,9 +25,9 @@ class SecurityService:
     def create_access_token(data: dict, expires_delta: timedelta | None = None):
         to_encode = data.copy()
         if expires_delta:
-            expire = datetime.utcnow() + expires_delta
+            expire = datetime.now(UTC) + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+            expire = datetime.now(UTC) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
         return encoded_jwt
