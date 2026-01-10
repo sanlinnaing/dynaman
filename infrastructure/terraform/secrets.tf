4f2b2a7b9f4fd7e3486f25d4f119a9c668051de5
San Lin Naing <sanlinnaing2008@gmail.com>
2026-01-10 23:14:25 +0900

    changed from SSM to SecretsManager

--- infrastructure/terraform/secrets.tf
+++ infrastructure/terraform/secrets.tf
@@ -1,21 +1,27 @@
-resource "aws_ssm_parameter" "mongodb_url" {
+resource "aws_secretsmanager_secret" "mongodb_url" {
   name        = "/${var.project_name}/${var.environment}/MONGODB_URL"
   description = "MongoDB Connection String"
-  type        = "SecureString"
-  value       = var.mongodb_url
-
+  
   tags = {
     Environment = var.environment
   }
 }
 
-resource "aws_ssm_parameter" "jwt_secret_key" {
+resource "aws_secretsmanager_secret_version" "mongodb_url" {
+  secret_id     = aws_secretsmanager_secret.mongodb_url.id
+  secret_string = var.mongodb_url
+}
+
+resource "aws_secretsmanager_secret" "jwt_secret_key" {
   name        = "/${var.project_name}/${var.environment}/JWT_SECRET_KEY"
   description = "JWT Secret Key for signing tokens"
-  type        = "SecureString"
-  value       = var.jwt_secret_key
-
+  
   tags = {
     Environment = var.environment
   }
+}
+
+resource "aws_secretsmanager_secret_version" "jwt_secret_key" {
+  secret_id     = aws_secretsmanager_secret.jwt_secret_key.id
+  secret_string = var.jwt_secret_key
 }
